f92042b221cb48739934ce2cd5aa404b5eab21a3
Isadora XPS <isadoravuongvan@gmail.com>
2024-10-25 17:19:42 +0200

    bash commit

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ import archiver from 'archiver';
 import { GITHUBTEMPLATES } from './constants.js';
 
 const app = express();
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
+// Set the views directory
+app.set('views', path.join(process.cwd(), 'views'));
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'index.html'));
-});
 
 
 const documents = [
@@ -120,14 +119,48 @@ const documents = [
     }
   },
   {
-    name: 'realisation',
+    name: 'certif_realisation',
     multipleRecords: false,
     titleForming: function(data) {
-      return `Attestation de réalisation ${data["code_fromprog"]} ${ymd(data["au (from Session)"])} - ${data["nom"]} ${data["prenom"]}`;
+      return `Certificat de réalisation ${data["code_fromprog"]} ${ymd(data["au"])} - ${data["nom"]} ${data["prenom"]}`;
+    },
+    template: 'certif_realisation.docx',
+    table: 'Inscriptions',
+    queriedField: 'recordId',
+    dataPreprocessing: function(data) {
+        data['du'] = new Date(data["du"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
+        data['au'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
+        data['dureeh_fromprog'] = data["dureeh_fromprog"]/3600;
+        data["assiduite"] = data["assiduite"] * 100;
+        data['nom'] = Array.isArray(data["nom"]) ? data["nom"][0].toUpperCase() : data["nom"].toUpperCase();
+        data['today'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
+        data['apaye'] = data.moyen_paiement && data.date_paiement;
+        data['acquit'] = data["paye"].includes("Payé")
+        ? `Acquittée par ${data.moyen_paiement.toLowerCase()} le ${(new Date(data.date_paiement)).toLocaleDateString('fr-FR')}`
+        : "";
+    }
+  },
+  {
+    name: 'attestation',
+    multipleRecords: false,
+    titleForming: function(data) {
+      return `Attestation de formation ${data["code_fromprog"]} ${ymd(data["au"])} - ${data["nom"]} ${data["prenom"]}`;
     },
     template: 'attestation.docx',
     table: 'Inscriptions',
     queriedField: 'recordId',
+    dataPreprocessing: function(data) {
+        data['du'] = new Date(data["du"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
+        data['au'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
+        data['dureeh_fromprog'] = data["dureeh_fromprog"]/3600;
+        data["assiduite"] = data["assiduite"] * 100;
+        data['nom'] = Array.isArray(data["nom"]) ? data["nom"][0].toUpperCase() : data["nom"].toUpperCase();
+        data['today'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
+        data['apaye'] = data.moyen_paiement && data.date_paiement;
+        data['acquit'] = data["paye"].includes("Payé")
+        ? `Acquittée par ${data.moyen_paiement.toLowerCase()} le ${(new Date(data.date_paiement)).toLocaleDateString('fr-FR')}`
+        : "";
+    }
   },
   {
     name: 'factures',
@@ -140,6 +173,12 @@ const documents = [
     queriedField: 'recordId',
   }
 ]
+
+
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(process.cwd(), 'index.html'), { documents: JSON.stringify(documents) });
+    res.render('index', { documents });
+});
 
 const handleReportGeneration = async (req, res, document) => {
   try {
@@ -192,13 +231,6 @@ const handleReportGeneration = async (req, res, document) => {
   }
 }
 
-// // Iterate over the documents array and create routes dynamically
-// documents.forEach(doc => {
-//   app.get(`/${doc.name}`, (req, res) => {
-//     handleReportGeneration(req, res, doc.templateUrl, doc.processData, doc.generateFileName);
-//   });
-// });
-
 app.get('/schemas', async (req, res) => {
   try {
     const schema = await getAirtableSchema();
@@ -227,370 +259,11 @@ app.get('/schemas', async (req, res) => {
   }
 });
 
-
-
-app.get('/catalogue', async (req, res) => {
-  // res.sendFile(path.join(process.cwd(), 'index.html'));
-  const table = "Sessions";
-  const view = "Grid view";
-  // const view = "Catalogue";
-  var { annee } = req.query;
-
-  if(!annee) { annee = new Date().getFullYear() + 1; }
-
-  // const formula = `AND(OR({année}=${annee},{année}=""), OR(FIND(lieuxdemij_cumul,"iège"),FIND(lieuxdemij_cumul,"visio")))`; 
-  const formula = `
-  OR(
-    AND(
-        {année}="", 
-        FIND(lieuxdemij_cumul,"intra")
-    ),
-    AND(
-        {année}=2025, 
-        FIND(lieuxdemij_cumul,"intra")=0
-    )
-  )`
-
-  try {
-    const data = await getAirtableRecords(table, view, formula, "du", "asc");
-    if (data) {
-      console.log('Data successfully retrieved:', data.records.length, "records");
-      // broadcastLog(`Data successfully retrieved: ${data.records.length} records`);
-    } else {
-      console.log('Failed to retrieve data.');
-      // broadcastLog('Failed to retrieve data.');
-    }
-    
-    // Generate and send the report
-    await generateAndDownloadReport(
-      GITHUBTEMPLATES + 'catalogue.docx', 
-      data, 
-      res,
-      'Catalogue des formations FSH ' + annee
-    );
-    // res.render('index', { title: 'Catalogue', heading: `Catalogue : à partir de ${table}/${view}` });
-  } catch (error) {
-    console.error('Error:', error);
-    // broadcastLog(`Error: ${error.message}`);
-    res.status(500).json({ success: false, error: error.message });
-  }
-  
-});
-
-app.get('/programme', async (req, res) => {
-  // res.sendFile(path.join(process.cwd(), 'index.html'));
-  const table="Sessions";
-  // const recordId="recAzC50Q7sCNzkcf";
-  const { recordId } = req.query;
-  
-  if (!recordId) {
-    return res.status(400).json({ success: false, error: 'Paramètre recordId manquant.' });
-  }
-  try {
-    const data = await getAirtableRecord(table, recordId);
-    if (data) {
-      console.log('Data successfully retrieved:', data.length);
-      // broadcastLog(`Data successfully retrieved: ${data.length} records`);
-    } else {
-      console.log('Failed to retrieve data.');
-      // broadcastLog('Failed to retrieve data.');
-    }
-
-    let newTitle = data["titre_fromprog"]
-    if(data["du"] && data["au"]) { newTitle+= `${ymd(data["du"])}-${data["au"] && ymd(data["au"])}`}
-
-    // Generate and send the report
-    await generateAndDownloadReport(
-      GITHUBTEMPLATES + 'programme.docx', 
-      data, 
-      res,
-      newTitle || "err titre prog"
-    );
-    // res.render('index', { title: `Générer un Programme pour ${recordId}`, heading: 'Programme' });
-  } catch (error) {
-    console.error('Error:', error);
-    // broadcastLog(`Error: ${error.message}`);
-    res.status(500).json({ success: false, error: error.message });
-  }
-  
-});  
-
-
-app.get('/devis', async (req, res) => {
-  // res.sendFile(path.join(process.cwd(), 'index.html'));
-  const table="Devis";
-  // const recordId="recAzC50Q7sCNzkcf";
-  const { recordId } = req.query;
-  
-  if (!recordId) {
-    return res.status(400).json({ success: false, error: 'Paramètre recordId manquant.' });
-  }
-  try {
-    const data = await getAirtableRecord(table, recordId);
-    if (data) {
-      console.log('Data successfully retrieved:', data.length);
-      // broadcastLog(`Data successfully retrieved: ${data.length} records`);
-    } else {
-      console.log('Failed to retrieve data.');
-      // broadcastLog('Failed to retrieve data.');
-    }
-
-    let newTitle = `DEVIS FSH ${data["id"]} `
-    // if(data["du"] && data["au"]) { newTitle+= `${ymd(data["du"])}-${data["au"] && ymd(data["au"])}`}
-    
-    // Generate and send the report
-    await generateAndDownloadReport(
-      GITHUBTEMPLATES + 'devis.docx', 
-      data, 
-      res,
-      newTitle
-    );
-    // res.render('index', { title: `Générer un Programme pour ${recordId}`, heading: 'Programme' });
-  } catch (error) {
-    console.error('Error:', error);
-    // broadcastLog(`Error: ${error.message}`);
-    res.status(500).json({ success: false, error: error.message });
-  }
-  
-});  
-
-
-app.get('/facture', async (req, res) => {
-  // res.sendFile(path.join(process.cwd(), 'index.html'));
-  const table="Inscriptions"; // Inscriptions
-  // const recordId="recdVx9WSFFeX5GP7";
-  const { recordId } = req.query;
-
-  var updatedInvoiceDate = false;
-  
-  if (!recordId) {
-    return res.status(400).json({ success: false, error: 'Paramètre recordId manquant.' });
-  }
-  try {
-    var data = await getAirtableRecord(table, recordId);
-    if (data) {
-      console.log('Data successfully retrieved:', data.length);
-      // broadcastLog(`Data successfully retrieved: ${data.length} records`);
-      if(data["date_facture"]) {
-        data["today"] = new Date(data["date_facture"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' });
-        updatedInvoiceDate = true;
-      }
-    } else {
-      console.log('Failed to retrieve data.');
-      // broadcastLog('Failed to retrieve data.');
-    }
-
-    // data['apaye'] = data.moyen_paiement && data.date_paiement;
-    data['acquit'] = data["paye"].includes("Payé")
-    ? `Acquittée par ${data.moyen_paiement.toLowerCase()} le ${(new Date(data.date_paiement)).toLocaleDateString('fr-FR')}`
-    : "";
-
-    function calculateCost(data) {
-      let cost;
-    
-      if (data["tarif_special"]) {
-        // If "tarif_special" is available, use it
-        cost = data["tarif_special"];
-      } else {
-        // Calculate the base cost, considering whether the person is accompanied
-        let baseCost;
-        if (data["accomp"]) {
-          baseCost = (data["Coût adhérent TTC (from Programme) (from Session)"] || 0) / 2;
-        } else {
-          if (data["Adhérent? (from Participant.e)"]) {
-            baseCost = data["Coût adhérent TTC (from Programme) (from Session)"] || 0;
-          } else {
-            baseCost = data["Coût non adhérent TTC (from Programme) (from Session)"] || 0;
-          }
-        }
-    
-        // Apply "rabais" if available
-        if (data["rabais"]) {
-          cost = baseCost * (1 - data["rabais"]);
-        } else {
-          cost = baseCost;
-        }
-      }
-    
-      return cost;
-    }
-
-    data["Montant"] = calculateCost(data)
-    console.log("Montant calc", data["Montant"])
-    data['montant'] = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(
-      parseFloat(data["Montant"]),
-    );  
-    console.log("montant", data["montant"])
-
-    // Generate and send the report
-    await generateAndDownloadReport(
-    // await generateAndSendZipReport(
-      GITHUBTEMPLATES + 'facture.docx', 
-      data, 
-      res,
-      `Facture ${data["id"]} ${data["nom"]} ${data["prenom"]}`
-    );
-    var updatedData = { 
-        total: data['Montant'].toString()
-      }
-    // TODO: update the record with the facture date
-    if(!updatedInvoiceDate) {
-      updatedData["date_facture"] = new Date().toLocaleDateString('fr-CA');
-    }
-    const updatedRecord = await updateAirtableRecord(table, recordId, updatedData);
-    if (updatedRecord) {
-      console.log('Facture date updated successfully:', updatedRecord.id);
-      // broadcastLog(`Facture date updated successfully: ${updatedRecord.id}`);
-    } else {
-      console.log('Failed to update facture date.');
-      // broadcastLog('Failed to update facture date.');
-    }
-    // res.render('index', { title: `Générer un Programme pour ${recordId}`, heading: 'Programme' });
-  } catch (error) {
-    console.error('Error:', error);
-    // broadcastLog(`Error: ${error.message}`);
-    res.status(500).json({ success: false, error: error.message });
-  } 
-  
-});  
-
-app.get('/attestformation', async (req, res) => {
-  const table = "Inscriptions";
-  const { recordId } = req.query;
-
-  if (!recordId) {
-    return res.status(400).json({ success: false, error: 'Paramètre recordId manquant.' });
-  }
-
-  try {
-    var data = await getAirtableRecord(table, recordId);
-    if (data) {
-      console.log('Data successfully retrieved:', data.length);
-      // broadcastLog(`Data successfully retrieved: ${data.length} records`);
-    } else {
-      console.log('Failed to retrieve data.');
-      // broadcastLog('Failed to retrieve data.');
-    }
-    
-    // date de l'attestation au dernier jour de la formation
-    data['today'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
-    data['apaye'] = data.moyen_paiement && data.date_paiement;
-    data['acquit'] = data["paye"].includes("Payé")
-    ? `Acquittée par ${data.moyen_paiement.toLowerCase()} le ${(new Date(data.date_paiement)).toLocaleDateString('fr-FR')}`
-    : "";
-    const newName = `Attestation de formation ${data["code_fromprog"]} ${new Date(data["au"]).toLocaleDateString('fr-FR').replace(/\//g, '-')} - ${data["nom"]} ${data["prenom"]}` || "err nom fact";
-    // console.log('Data retrieved in /realisation:', data);
-    // console.log('Code from prog:', data["code_fromprog"]);
-    // console.log('Date from session:', data["au (from Session)"]);
-    // console.log('New report name:', newName);
-    
-    // Generate and send the report
-    await generateAndDownloadReport(
-    // await generateAndSendZipReport(
-      GITHUBTEMPLATES + 'attestation.docx', 
-      data, 
-      res,
-      newName
-    );
-    // http://localhost:3000/realisation?recordId=rec9ZMibFvLaRuTm7
-
-    // res.render('index', { title: `Générer un Programme pour ${recordId}`, heading: 'Programme' });
-  } catch (error) {
-    console.error('Error:', error);
-    // broadcastLog(`Error: ${error.message}`);
-    res.status(500).json({ success: false, error: error.message });
-  } 
-});
-
-
-app.get('/realisation', async (req, res) => {
-  const table = "Inscriptions";
-  const { recordId } = req.query;
-
-  if (!recordId) {
-    return res.status(400).json({ success: false, error: 'Paramètre recordId manquant.' });
-  }
-
-  try {
-    var data = await getAirtableRecord(table, recordId);
-    if (data) {
-      console.log('Data successfully retrieved:', data.length);
-      // broadcastLog(`Data successfully retrieved: ${data.length} records`);
-    } else {
-      console.log('Failed to retrieve data.');
-      // broadcastLog('Failed to retrieve data.');
-    }
-    console.log("data:NOM", data.nom)
-    // date de l'attestation au dernier jour de la formation
-    data['du'] = new Date(data["du"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
-    data['au'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
-    data['dureeh_fromprog'] = data["dureeh_fromprog"]/3600;
-    data["assiduite"] = data["assiduite"] * 100;
-    data['nom'] = Array.isArray(data["nom"]) ? data["nom"][0].toUpperCase() : data["nom"].toUpperCase();
-    data['today'] = new Date(data["au"]).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })
-    data['apaye'] = data.moyen_paiement && data.date_paiement;
-    data['acquit'] = data["paye"].includes("Payé")
-    ? `Acquittée par ${data.moyen_paiement.toLowerCase()} le ${(new Date(data.date_paiement)).toLocaleDateString('fr-FR')}`
-    : "";
-    const newName = `Certificat de réalisation ${data["code_fromprog"]} ${new Date(data["au"]).toLocaleDateString('fr-FR').replace(/\//g, '-')} - ${data["nom"]} ${data["prenom"]}` || "err nom fact";
-    // console.log('Data retrieved in /realisation:', data);
-    // console.log('Code from prog:', data["code_fromprog"]);
-    // console.log('Date from session:', data["au (from Session)"]);
-    // console.log('New report name:', newName);
-    
-    // Generate and send the report
-    await generateAndDownloadReport(
-    // await generateAndSendZipReport(
-      GITHUBTEMPLATES + 'certif_realisation.docx', 
-      data, 
-      res,
-      newName
-    );
-    // http://localhost:3000/realisation?recordId=rec9ZMibFvLaRuTm7
-
-    // res.render('index', { title: `Générer un Programme pour ${recordId}`, heading: 'Programme' });
-  } catch (error) {
-    console.error('Error:', error);
-    // broadcastLog(`Error: ${error.message}`);
-    res.status(500).json({ success: false, error: error.message });
-  } 
-});
-
-app.get('/factures', async (req, res) => {
-  const table = "Sessions";
-  const sessionId = "recxEooSpjiO0qbvQ";
-  // const { sessionId } = req.query
-
-  const session = await getAirtableRecord("Sessions", sessionId);
-  const inscrits = session["Inscrits"];
-
-  var files = [];
-
-  await Promise.all(inscrits.map(async id => {
-    const data = await getAirtableRecord("tblxLakvfLieKsRyH", id);
-    const fileName = `Facture ${data["id"]} ${data["nom"]} ${data["prenom"]}.docx`;
-    console.log(data);
-    const buffer = await generateReportBuffer(
-      GITHUBTEMPLATES+"facture.docx",
-      data
-    );
-
-    if (buffer) {
-      files.push({ fileName, buffer });
-    } else {
-      console.error(`Invalid buffer for file: ${fileName}`);
-    }
-  }));
-
-  if (files.length > 0) {
-    await createZipArchive(files, res, 'all_reports.zip');
-  } else {
-    res.status(500).json({ success: false, error: 'No valid files to archive.' });
-  }
-
-  // TODO: update all records with the facture date
-  // const updateAirtableRecords = async (table, recordIds, data) => {
-  
+// Iterate over the documents array and create routes dynamically
+documents.forEach(doc => {
+  app.get(`/${doc.name}`, (req, res) => {
+    handleReportGeneration(req, res, doc);
+  });
 });
 
 
@@ -770,10 +443,10 @@ async function uploadReportToBlobStorage(fileName, buffer) {
 // Start the server
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port http://localhost:${process.env.PORT || 3000}/`);
-  console.log("TESTS FACTURE")
-  console.log(`Impayée : http://localhost:${process.env.PORT || 3000}/facture?recordId=rechdhSdMTxoB8J1P`);
-  console.log(`Rabais : http://localhost:${process.env.PORT || 3000}/facture?recordId=recFYDogCDybfujfd`);
-  console.log(`Accomp : http://localhost:${process.env.PORT || 3000}/facture?recordId=recvrbZmRuUCgHrFK`);
-  console.log(`Payée : http://localhost:${process.env.PORT || 3000}/facture?recordId=recLM3WRAiRYNPZ52`);
+//   console.log("TESTS FACTURE")
+//   console.log(`Impayée : http://localhost:${process.env.PORT || 3000}/facture?recordId=rechdhSdMTxoB8J1P`);
+//   console.log(`Rabais : http://localhost:${process.env.PORT || 3000}/facture?recordId=recFYDogCDybfujfd`);
+//   console.log(`Accomp : http://localhost:${process.env.PORT || 3000}/facture?recordId=recvrbZmRuUCgHrFK`);
+//   console.log(`Payée : http://localhost:${process.env.PORT || 3000}/facture?recordId=recLM3WRAiRYNPZ52`);
 });
 
