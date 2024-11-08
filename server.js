dd0d00e504484cd08257b1ac16674726d033270b
IVV FSH <isadoravuongvan@gmail.com>
2024-11-08 14:11:11 +0100

    auto commit - fsh account on dell

--- server.js
+++ server.js
@@ -930,12 +930,13 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Accomp : http://localhost:${process.env.PORT || 3000}/facture?recordId=recvrbZmRuUCgHrFK`);
   console.log(`Payée : http://localhost:${process.env.PORT || 3000}/facture?recordId=recLM3WRAiRYNPZ52`);
   for (const doc of documents) {
-    // console.log(`http://localhost:${process.env.PORT || 3000}/make/${doc.name}`);
     console.log(doc.name.toUpperCase())
     if(doc.examples) {
       doc.examples.forEach(example => {
         console.log(`http://localhost:${process.env.PORT || 3000}/make/${doc.name}?recordId=${example.recordId}`, example.desc);
       });
+    } else {
+      console.log(`http://localhost:${process.env.PORT || 3000}/make/${doc.name}`);
     }
   }
   console.log("Documents session ------")
