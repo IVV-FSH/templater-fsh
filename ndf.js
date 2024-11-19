8d58fd4131b278dde55ec58d988945f897755e93
IVV FSH <isadoravuongvan@gmail.com>
2024-11-19 13:07:51 +0100

    auto commit - fsh account on dell

---
+++ ndf.js
@@ -0,0 +1,77 @@
+import dotenv from 'dotenv';
+
+import axios from 'axios';
+
+import nodemailer from 'nodemailer';
+
+dotenv.config();
+const AIRTABLE_API_KEY = process.env.AIRTABLE_TOKEN; // Get API key from environment variable
+const AIRTABLE_BASE_ID = 'appaMzGMfnXGbGBd2'; // Replace with your Airtable Base ID
+const AUTH_HEADERS = {
+	Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+};
+
+export const getAirtableRecords = async (table, view = null, formula = null, sortField = null, sortDir = null) => {
+	try {
+	  let url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(table)}`;
+	  const params = [];
+	  if (formula) {
+		params.push(`filterByFormula=${formulaFilter(formula)}`);
+	  }
+	  if (view) {
+		params.push(`view=${encodeURIComponent(view)}`);
+	  }
+	  if (sortField) {
+		let sortParam = `sort%5B0%5D%5Bfield%5D=${sortField}`;
+		if (sortDir && ['asc', 'desc'].includes(sortDir)) {
+			sortParam += `&sort%5B0%5D%5Bdirection%5D=${sortDir}`;
+		}
+		params.push(sortParam);
+	  }
+	  if (params.length > 0) {
+		url += `?${params.join('&')}`;
+	  }
+	  console.log(`Fetching records from URL: ${url}`);
+	  // broadcastLog(`Fetching records from URL: ${url}`); 
+	
+	  const response = await axios.get(url, {
+		headers: AUTH_HEADERS,
+	  });
+  
+	//   if lenth of records is 0, return empty array
+	  if(response.data.records.length == 0) {
+		return [];
+	  }
+	  let processedData = response.data.records.map(record => record.fields);
+	//   console.log(`Fetched records: ${JSON.stringify(processedData)}`);
+	//   processedData = processedData.map(record => {
+	// 	if (Array.isArray(record)) {
+	// 	  return record.join(', ');
+	// 	}
+	// 	return record;
+	//   });
+  
+	processedData = processedData.map(record => processFieldsForDocx(
+		record,
+		airtableMarkdownFields
+	));
+	processedData['today'] = new Date().toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric', timeZone: 'Europe/Paris'})
+
+	return { records: processedData };
+	} catch (error) {
+	  console.error(error);
+	  throw new Error('Error retrieving data from Airtable.');
+	}
+  };
+
+
+
+export default async function envoiNdf() {
+    console.log('Envoi NDF');
+    const ndfs = await getAirtableRecords("Dépenses", "Main View", `{remboursé_compta} = 0`);
+
+    // Envoyer un mail à responsable, copie la compta
+    // Joindre tous les justificatifs ("Justificatifs (photo/scan)")
+    // Demander de valider les dépenses (par écrit puis TODO: en cliquant sur un lien qui valide et envoie à la compta)
+
+}
